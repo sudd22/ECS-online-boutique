f0353399354b316d9aecce9a50401e10042e97f4
sudd22 <seudali200@gmail.com>
2026-07-17 22:35:26 +0100

    fis

---
+++ terraform/modules/fis/remediation_lambda.tf
@@ -0,0 +1,21 @@
+data "archive_file" "remediation_zip" {
+  type        = "zip"
+  source_file = "${path.module}/scripts/remediation_handler.py"
+  output_path = "${path.module}/lambda_payload.zip"
+}
+
+resource "aws_lambda_function" "remediation" {
+  function_name    = "${var.env}-devops-agent-remediation"
+  handler          = "remediation_handler.lambda_handler"
+  runtime          = "python3.11"
+  role             = aws_iam_role.remediation_lambda_role.arn
+  filename         = data.archive_file.remediation_zip.output_path
+  source_code_hash = data.archive_file.remediation_zip.output_base64sha256
+
+  environment {
+    variables = {
+      ECS_CLUSTER_NAME = var.ecs_cluster_name
+      ECS_SERVICE_NAME = "${var.env}-b2b-monolith-service"
+    }
+  }
+}
