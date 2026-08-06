2884589b9d4313f21794b97fdcded67d90308a87
sudd22 <seudali200@gmail.com>
2026-08-06 19:34:20 +0100

    route53

---
+++ terraform/modules/route53/main.tf
@@ -0,0 +1,29 @@
+data "aws_route53_zone" "primary" {
+  name         = var.domain_name
+  private_zone = false
+}
+
+resource "aws_route53_record" "record_a" {
+  zone_id = data.aws_route53_zone.primary.zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = var.alb_dns_name
+    zone_id                = var.alb_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "record_aaaa" {
+  zone_id = data.aws_route53_zone.primary.zone_id
+  name    = var.domain_name
+  type    = "AAAA"
+
+  alias {
+    name                   = var.alb_dns_name
+    zone_id                = var.alb_zone_id
+    evaluate_target_health = false
+  }
+}
+
